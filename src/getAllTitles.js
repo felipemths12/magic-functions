c6e8977e03aaa7960fc82cca55c5602884f37895
unknown <felipe.matheussms@gmail.com>
2024-09-16 21:54:12 -0300

    desafio 1 concluido

--- src/getAllTitles.js
+++ src/getAllTitles.js
@@ -1,7 +1,11 @@
 const movies = require("../data/movies");
 
 const getAllTitles = (movies) => {
- 
+ const result = movies.map((movie) => {
+  return movie.titulo
+ });
+
+ return result
 }
 
 module.exports = {
