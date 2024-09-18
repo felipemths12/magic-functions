b4588e1260493e7be161c74985037763a3c50d91
unknown <felipe.matheussms@gmail.com>
2024-09-18 19:27:11 -0300

    desafio 2 concluido

--- src/getMovieById.js
+++ src/getMovieById.js
@@ -1,7 +1,21 @@
 const movies = require("../data/movies");
 
 function getMovieById(id) {
-  
+ if (typeof id !== "number"){
+  return undefined
+ }
+
+ if (id === 0){
+  return undefined
+ }
+
+ if (id < 0){
+  return undefined
+ }
+ const result = movies.find((movie) => movie.id === id);
+
+ return result
+
 }
 
 module.exports = {
