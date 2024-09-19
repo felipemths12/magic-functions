aab98d6c7a1955e2149f4b17ec3e680e6616ba0c
unknown <felipe.matheussms@gmail.com>
2024-09-18 21:15:28 -0300

    desafio 4 concluido

--- src/bestRatedFilm.js
+++ src/bestRatedFilm.js
@@ -2,6 +2,12 @@ const movies = require("../data/movies");
 
 const bestRatedFilm = (movies) => {
   
+  
+  const result = movies.sort((a, b) => {
+    return b.avaliacao - a.avaliacao
+  }).shift()
+
+  return result
 }
 
 module.exports = {
