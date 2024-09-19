77dda07e4f42ff8642f53b5a313f27fb05a28c65
unknown <felipe.matheussms@gmail.com>
2024-09-19 18:31:00 -0300

    desafio 3 concluido

--- src/moviesWithActor.js
+++ src/moviesWithActor.js
@@ -1,7 +1,12 @@
 const movies = require("../data/movies");
 
 function moviesWithActor(ator) {
- 
+const result = movies.filter((movie) => {
+  return movie.elenco.includes(ator)
+});
+
+return result
+
 }
 
 module.exports = {
