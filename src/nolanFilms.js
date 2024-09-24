c221c68e0f558a4467efe0ad1d44a9a3c17e5ea9
unknown <felipe.matheussms@gmail.com>
2024-09-23 21:22:39 -0300

    desafio 8 concluido

--- src/nolanFilms.js
+++ src/nolanFilms.js
@@ -1,7 +1,17 @@
 const movies = require("../data/movies");
 
 const nolanFilms = (movies) => {
- 
+ const result = movies.filter((movie) => {
+  if (movie.diretor.includes("Christopher Nolan")){
+    return movie
+  }
+ }).sort((a, b) => {
+  return b.avaliacao - a.avaliacao
+ }).map((movie) => {
+  return movie
+ });
+
+ return result
 }
 
 module.exports = {
