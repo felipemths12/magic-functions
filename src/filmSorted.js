c9011330d093c3a1d1ed5d5f87ea46f4b87144b0
unknown <felipe.matheussms@gmail.com>
2024-09-23 21:20:30 -0300

    desafio 6 concluido

--- src/filmSorted.js
+++ src/filmSorted.js
@@ -1,7 +1,13 @@
 const movies = require("../data/movies");
 
 const filmSorted = (movies) => {
-  
+  const result = movies.sort((a, b) => {
+    return a.anoLancamento - b.anoLancamento;
+  }).map((movie) => {
+    return movie
+  });
+
+  return result
 }
 
 module.exports = {
