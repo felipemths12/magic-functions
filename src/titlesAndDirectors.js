290500e75a6d28cf5fc95960e47167bc54521b3e
unknown <felipe.matheussms@gmail.com>
2024-09-23 21:21:17 -0300

    desafio 5 concluido

--- src/titlesAndDirectors.js
+++ src/titlesAndDirectors.js
@@ -1,8 +1,12 @@
-const { movies } = require("../data/list1");
+const movies  = require("../data/movies");
 
 const titlesAndDirectors = (movies) => {
- 
+ movies.filter((movie) => {
+  return movie.titulo && movie.diretor
+ }).forEach((movie) => { console.log(`Título: ${movie.titulo} - Diretor: ${movie.diretor}`)})
 }
+
+
 
 module.exports = {
   titlesAndDirectors
