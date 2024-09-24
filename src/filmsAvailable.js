cd1d50d57bd77eb4d31b0f552929ab2b4ecdec62
unknown <felipe.matheussms@gmail.com>
2024-09-23 21:22:05 -0300

    desafio 7 concluido

--- src/filmsAvailable.js
+++ src/filmsAvailable.js
@@ -1,7 +1,17 @@
 const movies = require("../data/movies");
 
 const filmsAvailable = (movies) => {
- 
+ const result = movies.filter((movie) => {
+  if (movie.disponivel === true){
+    return movie
+  } else {
+    return undefined
+  }
+ }).map((movie) => {
+  return `${movie.titulo}`
+ })
+
+ return result
 }
 
 module.exports = {
